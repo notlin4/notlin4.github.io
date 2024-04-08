4849484b728bdd504d5b9f537214d4babf7d7224
Tsaitou7361 <Tsaitou7361@users.noreply.github.com>
2024-04-08 13:51:34 +0000

    deploy: 8f12588681a1b807ed5c988650fe39bdb0b60e80

---
+++ assets/mailbox.html-9a1f3e92.js
@@ -0,0 +1,23 @@
+import{_ as t,M as n,p as d,q as r,R as e,N as s,t as c,a1 as i}from"./framework-5866ffd3.js";const o={},l=e("h1",{id:"站內信箱",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#站內信箱","aria-hidden":"true"},"#"),c(" 站內信箱")],-1),x={href:"https://www.mc-list.xyz/mailbox",target:"_blank",rel:"noopener noreferrer"},h=e("code",null,"/mailbox",-1),p=i(`<div class="custom-container tip"><p class="custom-container-title">功能已轉移</p><p>站內信箱部分功能已轉移至 <a href="/other/bind-discord">綁定 Discord</a></p></div><h2 id="每月伺服器報告" tabindex="-1"><a class="header-anchor" href="#每月伺服器報告" aria-hidden="true">#</a> 每月伺服器報告</h2><div class="language-text" data-ext="text"><pre class="language-text"><code>您的伺服器 無名氏伺服器 在 2月期間收到了 11 次投票，並有 170 次首頁列表點擊次數，感謝您的使用。
+</code></pre></div><h2 id="對您的伺服器發表評論" tabindex="-1"><a class="header-anchor" href="#對您的伺服器發表評論" aria-hidden="true">#</a> 對您的伺服器發表評論</h2><div class="language-text" data-ext="text"><pre class="language-text"><code>猫又おにぎり 對您的伺服器 無名氏伺服器 做出了評價：「這是一個新版評價ㄉ測試訊息！」評價等級：正評
+</code></pre></div><h2 id="伺服器新增通知" tabindex="-1"><a class="header-anchor" href="#伺服器新增通知" aria-hidden="true">#</a> 伺服器新增通知</h2><div class="language-text" data-ext="text"><pre class="language-text"><code>您的伺服器 無名氏伺服器 已經成功建立囉！
+請點我前往伺服器擁有者驗證！
+以下就是您的伺服器介紹網址 https://www.mc-list.xyz/1/info
+如果您有使用上的任何問題，可以直接回覆此封信件，管理員就會回覆您唷！
+</code></pre></div><h2 id="伺服器移除紀錄" tabindex="-1"><a class="header-anchor" href="#伺服器移除紀錄" aria-hidden="true">#</a> 伺服器移除紀錄</h2><div class="language-text" data-ext="text"><pre class="language-text"><code>您的伺服器 test 已經於 2022/11/27 22:13:15 刪除成功，感謝您的使用。
+
+以下為您伺服器的紀錄：
+ＩＰ：xxx.xxx.xxx.xxx
+介紹：test
+</code></pre></div><h2 id="年度統計" tabindex="-1"><a class="header-anchor" href="#年度統計" aria-hidden="true">#</a> 年度統計</h2><div class="language-text" data-ext="text"><pre class="language-text"><code>2022 統計資料已經出爐囉！歡迎點選連結查看！haer0248.me/1166/
+
+2022 對使用者的介面上有做出了許多改動，還希望這些小改動有幫助到使用者更方便使用我們的網站
+另外若對網站上有任何的意見及問題，不吝加入我們的 Discord 來告知我們。
+</code></pre></div><h2 id="網站公告" tabindex="-1"><a class="header-anchor" href="#網站公告" aria-hidden="true">#</a> 網站公告</h2><div class="language-text" data-ext="text"><pre class="language-text"><code>為方便使用者接收來自 Minecraft 伺服器列表的通知（伺服器狀態、評價、廣告投放等），我們新增了 Discord 機器人通知功能！
+只要前往 我的伺服器 頁面進行綁定，就可以囉！
+
+我們不會使用機器人做廣告、宣傳用途，僅通知用。
+同時也只有 Minecraft 伺服器列表#5483 (1080776350077554708) 為官方通知機器人。
+
+另外因為 Discord 的限制，必須加入我們的 Discord 伺服器才可以正常收到通知（還要記得開啟私人訊息）。
+</code></pre></div>`,13);function u(g,_){const a=n("ExternalLinkIcon");return d(),r("div",null,[l,e("p",null,[e("a",x,[h,s(a)])]),p])}const m=t(o,[["render",u],["__file","mailbox.html.vue"]]);export{m as default};
